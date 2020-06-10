52af311ed6c12216b726b8a90b5ff900cac05fa3
Abhishek <abhranja@gmail.com>
2020-06-10 16:39:09 +0530

    correcting typo

--- slidingWindow_424.java
+++ slidingWindow_424.java
@@ -1,10 +1,3 @@
-// Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
-
-// In one operation, you can choose any character of the string and change it to any other uppercase English character.
-
-// Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
-
-
 import java.util.*;
 class Solution {
     public int characterReplacement(String s, int k) {
@@ -13,10 +6,10 @@ class Solution {
         while(end!=s.length()){
             char endChar = s.charAt(end);
             charFrequency.put(endChar,charFrequency.getOrDefault(endChar,0)+1);
-            maxFrequency = Math.max(maxFrequency,charFrequency.get(end));
-            while(end-start+1-maxFreq > k){
-            	char startChar = charFrequency.charAt(start);
-            	charFrequency.put(startChar,charFrequency.get(start)-1);
+            maxFrequency = Math.max(maxFrequency,charFrequency.get(endChar));
+            while(end-start+1-maxFrequency > k){
+            	char startChar = s.charAt(start);
+            	charFrequency.put(startChar,charFrequency.get(startChar)-1);
             	start++;
             }
 
